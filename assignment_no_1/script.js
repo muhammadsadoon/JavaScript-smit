ffa8388fef0c311e4691b89c1dc7e556804e8a7e
sadoon <sadoonmuhammad8@gmail.com>
2025-05-08 22:34:44 +0500

    my assignment number is completed

---
+++ assignment_no_1/script.js
@@ -0,0 +1,59 @@
+// program no 1:Write a script to greet your website visitor using JS alert box:
+alert("welcome to my page !!");
+
+//program no 2:Write a script to display following message on your web page:
+alert("Error ! Please enter the valid password...");
+
+//program no 3:Write a script to display following message on your web page: (Hint : Use line break)
+alert("welcome to my JS loading page \n Happy coding...!");
+
+// program no 4:. Write a script to display following message on your web page: (Hint : Use line break)
+alert("Welcome to JS Load");
+alert("Happy coding..!");
+
+//program no 5:Generate the following message through browser’s developer console:
+var str = "Hello... I can run JS through my web browser's console";
+console.log("alert('"+str+"')")
+alert(str)
+
+//program no 6:Declare a variable called username
+var username;
+
+
+//program no 7:Declare a variable called myName & assign to it a string that represents your Full Name.
+var myName = "Muhammad Sadoon";
+
+//program no 8: Write a script to
+        // a. declare a JS variable, titled message
+        // b. assign “Hello World” to the variable message
+        // c. display the message in alert box
+
+var titled;
+titled = "Hello world";
+alert(titled);
+
+//program no 9: Write a script to save student’s bio data in JS variables and show the data in console (Name, Age, Institute, Course)
+var Name = "saad";
+var Age = 19;
+var Institute = "Saylani Mass training center";
+var Course = "Modren web and app development";
+console.log("Name: "+Name+"\nAge: "+Age +"\nInstitude: "+Institute + "\nCourses: "+Course);
+
+//program no 10: Declare a variable called age & assign to it your age. Show your age in console.
+var studentAge = 20;
+console.log("The student age is "+studentAge);
+
+//program no 11:  Fortune Teller: Store the following into variables: number of children, partner’s name, geographic location, and job title. Output your fortune like:
+var jobTitle = "MERN STACK Developer";
+var location = "Pakistan";
+var partnerName = "saira";
+var numberOfChildren = 3;
+console.log("you as a "+ jobTitle + " in "+location + " and you will marrried "+partnerName+" with " +numberOfChildren+" Kides");
+
+//program no 12: Declare a variable called email & assign to it a string that represents your Email Address (e.g. abc@xyz.com).
+var email = "sadoon@gmail.com";
+console.log("my email is "+email);
+
+//program no 13:Declare a variable called book & give it the value “A smarterway to learn JavaScript”. Display the following message in an alert box:
+var book = "A smarterway to learn JavaScript";
+alert(book);
